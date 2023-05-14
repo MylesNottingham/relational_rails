e2a30ffe36904bfa646dea48d79381c27d7441c3
Myles Nottingham <MylesNottingham@gmail.com>
2023-05-14 13:38:07 -0600

    Test: Refactor test for scalability.

--- spec/features/resorts/index_spec.rb
+++ spec/features/resorts/index_spec.rb
@@ -34,12 +34,14 @@ RSpec.describe "Resort Index Page" do
     end
 
     it "shows names of all resorts with creation date ordered by creation timestamp" do
-      expect(page).to have_content(
-        "#{@mammoth.name} - #{@mammoth.created_at}\n#{@snow_summit.name} - #{@snow_summit.created_at}"
-      )
-      expect(page).to have_content(
-        "#{@snow_summit.name} - #{@snow_summit.created_at}\n#{@bear_mountain.name} - #{@bear_mountain.created_at}"
-      )
+      actual = page.all("h3").map(&:text)
+      expected = [
+        "#{@mammoth.name} - #{@mammoth.created_at}",
+        "#{@snow_summit.name} - #{@snow_summit.created_at}",
+        "#{@bear_mountain.name} - #{@bear_mountain.created_at}"
+      ]
+      
+      expect(actual).to eq(expected)
     end
   end
 end
