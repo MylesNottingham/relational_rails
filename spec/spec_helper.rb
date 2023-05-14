9df2011f53762b9817e2fdc8199bac78adbd6cc8
Myles Nottingham <MylesNottingham@gmail.com>
2023-05-13 20:53:30 -0600

    Test: Add seed data to spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,3 +92,19 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+def test_seed
+  @mammoth = Resort.create!(name: "Mammoth", max_capacity: 1000, open_for_season: true)
+  @snow_summit = Resort.create!(name: "Snow Summit", max_capacity: 600, open_for_season: true)
+  @bear_mountain = Resort.create!(name: "Bear Mountain", max_capacity: 500, open_for_season: false)
+
+  @nick = @mammoth.guests.create!(name: "Nick", pass_holder: true, days_active: 20)
+  @megan = @mammoth.guests.create!(name: "Megan", pass_holder: true, days_active: 10)
+  @brian = @mammoth.guests.create!(name: "Brian", pass_holder: false, days_active: 15)
+
+  @sal = @snow_summit.guests.create!(name: "Sal", pass_holder: true, days_active: 5)
+  @ali = @snow_summit.guests.create!(name: "Ali", pass_holder: false, days_active: 10)
+
+  @mike = @bear_mountain.guests.create!(name: "Mike", pass_holder: true, days_active: 5)
+  @molly = @bear_mountain.guests.create!(name: "Molly", pass_holder: false, days_active: 10)
+end
