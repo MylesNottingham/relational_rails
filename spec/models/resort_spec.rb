e70ab0f83dad2fda84d9bee7a599c4847b542813
Myles Nottingham <MylesNottingham@gmail.com>
2023-05-13 20:54:46 -0600

    Test: Refactor Validations for Resort model.

--- spec/models/resort_spec.rb
+++ spec/models/resort_spec.rb
@@ -7,6 +7,6 @@ RSpec.describe Resort do
 
   describe "validations" do
     it {should validate_presence_of(:name)}
-    it {should validate_presence_of(:max_capacity)}
+    it {should validate_numericality_of(:max_capacity)}
   end
 end
