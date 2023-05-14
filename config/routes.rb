e2ebdad1df0fb4957a98cae286c097be8775e256
Myles Nottingham <MylesNottingham@gmail.com>
2023-05-13 21:01:45 -0600

    Feat: Add Routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   get "/resorts", to: "resorts#index"
   get "/resorts/:id", to: "resorts#show"
+
+  get "/resorts/:id/guests", to: "resorts/guests#index"
+
+  get "/guests", to: "guests#index"
+  get "/guests/:id", to: "guests#show"
 end
